ef7b6c928bff693e03080085fc5f7ab986273369
JuliaMichaela <juliaperez@yandex.ru>
2024-05-17 17:17:39 +0300

    edited Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,14 @@ import './Header.css';
 
 export default function Header() {
     return (
-        <div>Header</div>
+        <div className={style.header}>
+            <img className={style.logo} src={logo} alt={"logo"} />
+            <div className={style.headerText}>Learning English Words</div>
+            <nav className={style.navigator}>
+                <div>Home</div>
+                <div>Game</div>
+                <div>Table</div>
+            </nav>
+        </div>
     )
 }
