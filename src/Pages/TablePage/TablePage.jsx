0fe15823abbda63e96a7dbb1e229779ac3ceaf69
JuliaMichaela <juliaperez@yandex.ru>
2024-05-23 23:47:52 +0300

    test

--- src/Pages/TablePage/TablePage.jsx
+++ src/Pages/TablePage/TablePage.jsx
@@ -4,6 +4,8 @@ export default function TablePage() {
     return (
         <div className={style.tableContainer}>
             <Table />
+            <Table />
+            <Table />
         </div>
     )
 }
