939892590912289edf1c9119b139aec48acf6015
JuliaMichaela <juliaperez@yandex.ru>
2024-06-20 23:10:31 +0300

    changed Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,11 @@ export default function Header() {
         <div className={style.header}>
             <img className={style.logo} src={logo} alt={"logo"} />
             <div className={style.headerText}>Learning English Words</div>
-            {/* <nav className={style.navigator}>
+            <nav className={style.navigator}>
                 <div>Home</div>
                 <div>Game</div>
                 <div>Table</div>
-            </nav> */}
+            </nav>
         </div>
     )
 }
