106c5d718f8f7991c5824b92f0498b35c0e6d022
JuliaMichaela <juliaperez@yandex.ru>
2024-07-08 23:00:15 +0300

    test

--- src/components/WordCard/WordCard.jsx
+++ src/components/WordCard/WordCard.jsx
@@ -3,7 +3,7 @@ import './WordCard.css'
 export default function WordCard() {
     return (
         <div className="card animation">
-            <div className="word">{props.engVersion}</div>
+            {/* <div className="word">{props.engVersion}</div> */}
             <div className="transcription">{props.transcription}</div>
             <button className={isVisible ? "btnVisible word" : "btnTranslate"} disabled={isVisible} onClick={handleShow} >
                 {isVisible ? props.rusVersion : "Проверить"}
