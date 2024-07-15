5f7ed6d604764a7a32bbff763a35e5c1a10296e5
JuliaMichaela <juliaperez@yandex.ru>
2024-07-15 22:52:21 +0300

    updated HomePage.jsx

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function HomePage() {
     return (
-        <div>HomePage</div>
+        <div className={style.container}>
+            <div className={style.row}>
+                <div className={style.column}>
+                    <img className={style.img} src={learn} alt="learn_language" />
+                </div>
+                <div className={style.column}>
+                    <h1>Learning English Words</h1>
+                    <p>Приложение для изучения иностранного языка и расширения словарного запаса.</p>
+                    <Link to="/game">Старт</Link>
+                </div>
+            </div>
+
+        </div>
     )
 }
