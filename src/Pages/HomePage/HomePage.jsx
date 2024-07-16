021dd0c557ed2e35ed68ce7ac5d2a1f5dc8112ca
JuliaMichaela <juliaperez@yandex.ru>
2024-07-16 23:06:34 +0300

    changed logo

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -4,9 +4,9 @@ export default function HomePage() {
     return (
         <div className={style.container}>
             <div className={style.row}>
-                <div className={style.column}>
+                {/* <div className={style.column}>
                     <img className={style.img} src={learn} alt="learn_language" />
-                </div>
+                </div> */}
                 <div className={style.column}>
                     <h1>Learning English Words</h1>
                     <p>Приложение для изучения иностранного языка и расширения словарного запаса.</p>
